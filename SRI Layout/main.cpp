4ecf454e9185b87b767562195519e936ab67363e
Jarrett vdB <jarrett@eduroam-169-233-238-238.ucsc.edu>
2017-02-13 19:34:08 -0800

    Initial Commit

---
+++ SRI Layout/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SRI Layout
+//
+//  Created by Jarrett on 2/13/17.
+//  Copyright © 2017 Jarrett. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
